45ec4c4a42e7f73a372ed0551fe288a4a2708098
Weston Ruter <weston@xwp.co>
2018-07-09 18:17:16 -0700

    Install service workers on login screen and other non-front/admin screens

--- wp-includes/default-filters.php
+++ wp-includes/default-filters.php
@@ -5,7 +5,8 @@
  * @package PWA
  */
 
-foreach ( array( 'wp_print_scripts', 'admin_print_scripts', 'customize_controls_print_scripts' ) as $filter ) {
+// Ensure service workers are printed on frontend, admin, Customizer, login, sign-up, and activate pages.
+foreach ( array( 'wp_print_scripts', 'admin_print_scripts', 'customize_controls_print_scripts', 'login_footer', 'after_signup_form', 'activate_wp_head' ) as $filter ) {
 	add_filter( $filter, 'wp_print_service_workers', 9 );
 }
 
