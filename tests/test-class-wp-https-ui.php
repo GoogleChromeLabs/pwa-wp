1c9d4222a221f1d6e342bdfe1c83660ee25b43c9
Ryan Kienstra <kienstraryan@gmail.com>
2018-07-26 00:25:14 -0500

    Add an option to upgrade insecure URLs.
    Per the designs in Issue 17,
    Add radio buttons to upgrade insecure URLs.

--- tests/test-class-wp-https-ui.php
+++ tests/test-class-wp-https-ui.php
@@ -34,19 +34,90 @@ class Test_WP_HTTPS_UI extends WP_UnitTestCase {
 	 */
 	public function test_init() {
 		$this->instance->init();
-		$this->assertEquals( 10, has_action( 'core_upgrade_preamble', array( $this->instance, 'render_ui' ) ) );
+		$this->assertEquals( 10, has_action( 'admin_init', array( $this->instance, 'init_admin' ) ) );
 	}
 
 	/**
-	 * Test render_ui.
+	 * Test register_settings.
 	 *
-	 * @covers WP_HTTPS_UI::render_ui()
+	 * @covers WP_HTTPS_UI::register_settings()
 	 */
-	public function test_render_ui() {
+	public function test_register_settings() {
+		global $new_whitelist_options, $wp_registered_settings;
+
+		$expected_settings = array(
+			'type'              => 'string',
+			'group'             => WP_HTTPS_UI::OPTION_GROUP,
+			'description'       => '',
+			'sanitize_callback' => array( $this->instance, 'sanitize_callback' ),
+			'show_in_rest'      => false,
+		);
+
+		$this->instance->register_settings();
+		$this->assertTrue( in_array( WP_HTTPS_UI::UPGRADE_HTTPS_OPTION, $new_whitelist_options['reading'], true ) );
+		$this->assertEquals(
+			$expected_settings,
+			$wp_registered_settings[ WP_HTTPS_UI::UPGRADE_HTTPS_OPTION ]
+		);
+
+		$this->assertTrue( in_array( WP_HTTPS_UI::UPGRADE_INSECURE_CONTENT_OPTION, $new_whitelist_options['reading'], true ) );
+		$this->assertEquals(
+			$expected_settings,
+			$wp_registered_settings[ WP_HTTPS_UI::UPGRADE_INSECURE_CONTENT_OPTION ]
+		);
+	}
+
+	/**
+	 * Test sanitize_callback.
+	 *
+	 * @covers WP_HTTPS_UI::sanitize_callback()
+	 */
+	public function test_sanitize_callback() {
+		// Assert that disallowed values aren't returned from the callback.
+		$this->assertEquals( null, $this->instance->sanitize_callback( 'foo string' ) );
+		$this->assertEquals( null, $this->instance->sanitize_callback( '2345' ) );
+
+		// Assert that allowed values are returned.
+		$this->assertEquals( '1', $this->instance->sanitize_callback( '1' ) );
+		$this->assertEquals( '0', $this->instance->sanitize_callback( '0' ) );
+	}
+
+	/**
+	 * Test add_settings_field.
+	 *
+	 * @covers WP_HTTPS_UI::add_settings_field()
+	 */
+	public function test_add_settings_field() {
+		global $wp_settings_fields;
+
+		$this->instance->add_settings_field();
+		$this->assertEquals(
+			array(
+				'id'       => WP_HTTPS_UI::SETTING_ID,
+				'title'    => 'HTTPS',
+				'callback' => array( $this->instance, 'render_settings' ),
+				'args'     => array(),
+			),
+			$wp_settings_fields[ WP_HTTPS_UI::OPTION_GROUP ]['default'][ WP_HTTPS_UI::SETTING_ID ]
+		);
+	}
+
+	/**
+	 * Test render_settings.
+	 *
+	 * @covers WP_HTTPS_UI::render_settings()
+	 */
+	public function test_render_settings() {
+		// Set the option values, which should appear in the <input type="radio"> elements.
+		update_option( WP_HTTPS_UI::UPGRADE_HTTPS_OPTION, WP_HTTPS_UI::OPTION_SELECTED_VALUE );
+		update_option( WP_HTTPS_UI::UPGRADE_INSECURE_CONTENT_OPTION, WP_HTTPS_UI::OPTION_SELECTED_VALUE );
 		ob_start();
-		$this->instance->render_ui();
+		$this->instance->render_settings();
 		$output = ob_get_clean();
-		$this->assertContains( 'Enable HTTPS', $output );
-		$this->assertContains( '<form method="post" action="', $output );
+
+		$this->assertContains( WP_HTTPS_UI::OPTION_SELECTED_VALUE, $output );
+		$this->assertContains( WP_HTTPS_UI::UPGRADE_HTTPS_OPTION, $output );
+		$this->assertContains( WP_HTTPS_UI::UPGRADE_INSECURE_CONTENT_OPTION, $output );
+		$this->assertContains( 'HTTPS is essential to securing your WordPress site, we strongly suggest enabling HTTPS on your site', $output );
 	}
 }
--- wp-includes/class-wp-https-ui.php
+++ wp-includes/class-wp-https-ui.php
@@ -11,56 +11,153 @@
 class WP_HTTPS_UI {
 
 	/**
-	 * The form action for this UI.
+	 * The option group for the offline page option.
 	 *
 	 * @var string
 	 */
-	const FORM_ACTION = 'update-core.php?action=do-enable-https';
+	const OPTION_GROUP = 'reading';
 
 	/**
-	 * The nonce action.
+	 * The option name to upgrade to https.
 	 *
 	 * @var string
 	 */
-	const NONCE_ACTION = 'wp_https_enable';
+	const UPGRADE_HTTPS_OPTION = 'should_upgrade_https';
 
 	/**
-	 * The name of the value to enable HTTPS.
+	 * The option name to ugprade insecure content.
 	 *
 	 * @var string
 	 */
-	const ENABLE_HTTPS_NAME = 'wp_enable_https';
+	const UPGRADE_INSECURE_CONTENT_OPTION = 'should_upgrade_insecure_content';
+
+	/**
+	 * The expected value if the option is enabled.
+	 *
+	 * @var string
+	 */
+	const OPTION_SELECTED_VALUE = '1';
+
+	/**
+	 * The ID of the settings section.
+	 *
+	 * @var string
+	 */
+	const SETTING_ID = 'wp_upgrade_https';
 
 	/**
 	 * Inits the class.
 	 */
 	public function init() {
-		add_action( 'core_upgrade_preamble', array( $this, 'render_ui' ) );
+		add_action( 'admin_init', array( $this, 'init_admin' ) );
 	}
 
 	/**
-	 * Renders the HTTPS UI in /wp-admin on the Dashboard > WordPress Updates page.
+	 * Initializes the admin tasks.
 	 */
-	public function render_ui() {
+	public function init_admin() {
+		$this->register_settings();
+		$this->add_settings_field();
+	}
+
+	/**
+	 * Registers the HTTPS settings.
+	 */
+	public function register_settings() {
+		$args = array(
+			'type'              => 'string',
+			'sanitize_callback' => array( $this, 'sanitize_callback' ),
+		);
+
+		register_setting(
+			self::OPTION_GROUP,
+			self::UPGRADE_HTTPS_OPTION,
+			$args
+		);
+
+		register_setting(
+			self::OPTION_GROUP,
+			self::UPGRADE_INSECURE_CONTENT_OPTION,
+			$args
+		);
+	}
+
+	/**
+	 * Sanitizes the raw setting value.
+	 *
+	 * @param string $raw_setting The setting before sanitizing it.
+	 * @return string|null The setting, or null if it's not valid.
+	 */
+	public function sanitize_callback( $raw_setting ) {
+		if ( self::OPTION_SELECTED_VALUE === $raw_setting || '0' === $raw_setting ) {
+			return $raw_setting;
+		}
+	}
+
+	/**
+	 * Adds a settings field to the 'Reading Settings' page.
+	 */
+	public function add_settings_field() {
+		add_settings_field(
+			self::SETTING_ID,
+			__( 'HTTPS', 'pwa' ),
+			array( $this, 'render_settings' ),
+			self::OPTION_GROUP
+		);
+	}
+
+	/**
+	 * Renders the HTTPS settings in /wp-admin on the Reading Settings page.
+	 */
+	public function render_settings() {
+		$upgrade_https_value           = get_option( self::UPGRADE_HTTPS_OPTION );
+		$upgrade_insecure_content      = get_option( self::UPGRADE_INSECURE_CONTENT_OPTION );
+		$https_more_details            = sprintf(
+			'<a href="%s">%s</a>',
+			'https://make.wordpress.org/support/user-manual/web-publishing/https-for-wordpress/',
+			esc_html__( 'More details', 'pwa' )
+		);
+		$insecure_content_more_details = sprintf(
+			'<a href="%s">%s</a>',
+			'https://developer.mozilla.org/en-US/docs/Web/Security/Mixed_content/How_to_fix_website_with_mixed_content',
+			esc_html__( 'More details', 'pwa' )
+		);
+
+		/**
+		 * Todo: change this description based on what insecure content is found.
+		 *
+		 * Have a separate description for when there's only passive insecure content.
+		 * And one for when there's active and passive insecure content.
+		 *
+		 * @see https://github.com/xwp/pwa-wp/issues/17#issuecomment-406188455
+		 */
+		/* Translators: %s: a link for more details */
+		$insecure_content_description = esc_html__( 'Your home page doesn’t contain insecure URLs. However, there may be URLs on other pages that could be blocked. %s', 'pwa' );
+
 		?>
-		<h2><?php esc_html_e( 'HTTPS', 'pwa' ); ?></h2>
-		<form method="post" action="<?php echo esc_url( self::FORM_ACTION ); ?>" name="enable-https" class="upgrade">
-			<?php wp_nonce_field( self::NONCE_ACTION ); ?>
-			<table class="form-table">
-				<tbody>
-					<tr class="option-site-visibility">
-						<th scope="row"><?php esc_html_e( 'Enable HTTPS', 'pwa' ); ?></th>
-						<td>
-							<fieldset><legend class="screen-reader-text"><span><?php esc_html_e( 'Options To Enable HTTPS', 'pwa' ); ?></span></legend>
-								<label><input name="<?php echo esc_attr( self::ENABLE_HTTPS_NAME ); ?>" type="radio" value="0" checked="checked"><?php esc_html_e( 'Yes', 'pwa' ); ?></label>
-								<label><input name="<?php echo esc_attr( self::ENABLE_HTTPS_NAME ); ?>" type="radio" value="0" checked="checked"><?php esc_html_e( 'No', 'pwa' ); ?></label>
-								<p class="description"><?php esc_html_e( 'Your site appears to support HTTPS', 'pwa' ); ?></p>
-							</fieldset>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-		</form>
+		<p class="description">
+			<?php
+			echo wp_kses_post( sprintf(
+				/* Translators: %s: a link for more details */
+				esc_html__( 'HTTPS is essential to securing your WordPress site, we strongly suggest upgrading to HTTPS on your site. %s', 'pwa' ),
+				$https_more_details
+			) );
+			?>
+		</p>
+		<p style="margin-top: 20px;"><strong><?php esc_html_e( 'HTTPS Upgrade', 'pwa' ); ?></strong></p>
+		<p class="description"><?php esc_html_e( 'Your site appears to support HTTPS', 'pwa' ); ?></p>
+		<p>
+			<label><input name="<?php echo esc_attr( self::UPGRADE_HTTPS_OPTION ); ?>" type="radio" <?php checked( $upgrade_https_value, self::OPTION_SELECTED_VALUE ); ?> value="<?php echo esc_attr( self::OPTION_SELECTED_VALUE ); ?>"><?php esc_html_e( 'Yes', 'pwa' ); ?></label>
+			<label><input name="<?php echo esc_attr( self::UPGRADE_HTTPS_OPTION ); ?>" type="radio" <?php checked( self::OPTION_SELECTED_VALUE !== $upgrade_https_value ); ?> value="0" ><?php esc_html_e( 'No', 'pwa' ); ?></label>
+		</p>
+
+		<p style="margin-top: 20px;"><strong><?php esc_html_e( 'Upgrade Insecure URLs', 'pwa' ); ?></strong></p>
+		<p class="description">
+			<?php echo wp_kses_post( printf( $insecure_content_description, $insecure_content_more_details ) ); ?>
+		<p>
+			<label><input name="<?php echo esc_attr( self::UPGRADE_INSECURE_CONTENT_OPTION ); ?>" type="radio" <?php checked( $upgrade_insecure_content, self::OPTION_SELECTED_VALUE ); ?> value="<?php echo esc_attr( self::OPTION_SELECTED_VALUE ); ?>"><?php esc_html_e( 'Yes', 'pwa' ); ?></label>
+			<label><input name="<?php echo esc_attr( self::UPGRADE_INSECURE_CONTENT_OPTION ); ?>" type="radio" <?php checked( self::OPTION_SELECTED_VALUE !== $upgrade_insecure_content ); ?> value="0" ><?php esc_html_e( 'No', 'pwa' ); ?></label>
+		</p>
 		<?php
 	}
 
