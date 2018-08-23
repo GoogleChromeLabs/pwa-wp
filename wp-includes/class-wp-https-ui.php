fc159a274426fae4d906fe596b39df86d0f4eb8a
Ryan Kienstra <kienstraryan@gmail.com>
2018-08-23 15:58:11 -0500

    Ensure is_array() and ! empty() before counting
    As Tonya pointed out,
    there can be an error if $total_urls_count isn't an array,
    or is_empty().
    Also, move the return statement higher to avoid
    assigning variables.

--- wp-includes/class-wp-https-ui.php
+++ wp-includes/class-wp-https-ui.php
@@ -171,19 +171,18 @@ class WP_HTTPS_UI {
 		</script>
 		<?php
 
-		$insecure_urls_option = get_option( WP_HTTPS_Detection::INSECURE_CONTENT_OPTION_NAME );
-		$all_insecure_urls    = $insecure_urls_option;
-		$total_urls_count     = count( $all_insecure_urls );
-		$insecure_content_id  = 'insecure-content';
-		$show_more_button_id  = 'view-urls';
-		$insecure_urls_class  = 'insecure-urls';
+		$all_insecure_urls = get_option( WP_HTTPS_Detection::INSECURE_CONTENT_OPTION_NAME );
+		$total_urls_count  = is_array( $all_insecure_urls) && ! empty( $all_insecure_urls ) ? count( $all_insecure_urls ) : 0;
 
 		// Exit if there is no insecure URL to display.
 		if ( ! $total_urls_count ) {
 			return;
 		}
 
-		$description = sprintf(
+		$insecure_content_id = 'insecure-content';
+		$show_more_button_id = 'view-urls';
+		$insecure_urls_class = 'insecure-urls';
+		$description         = sprintf(
 			/* translators: %s is a link for more details */
 			__( 'We found content on your site that wasn&#39;t loading correctly over HTTPS. While we will try to fix these links automatically, you might check to be sure your pages work as expected. %s', 'pwa' ),
 			sprintf(
