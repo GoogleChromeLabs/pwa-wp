c4c88b3f330465dfb8621d30d2511a5ba494b15c
Weston Ruter <westonruter@google.com>
2018-10-26 19:40:40 -0700

    Fix promise chain (undo 69eb3951); use 202 status, follow redirects, copied mode

--- amp/amp-service-worker-offline-commenting.js
+++ amp/amp-service-worker-offline-commenting.js
@@ -4,37 +4,40 @@
 	const errorMessages = ERROR_MESSAGES;
 
 	const commentHandler = ( { event } ) => {
-		const clone = event.request.clone();
+		const clonedRequest = event.request.clone();
 		return fetch( event.request )
 			.then( ( response ) => {
 				// @todo Make sure that 409 etc. error still work as expected.
 				return response;
 			} )
-			.catch( () => clone.blob() )
-			.then( ( body ) => {
-				const queuedRequest = new Request( event.request.url, {
-					method: event.request.method,
-					headers: event.request.headers,
-					mode: 'same-origin',
-					credentials: event.request.credentials,
-					referrer: event.request.referrer,
-					redirect: 'manual',
-					body: body
-				} );
+			.catch( () => {
+				return clonedRequest.blob().then( ( body ) => {
+					const queuedRequest = new Request( event.request.url, {
+						method: event.request.method,
+						headers: event.request.headers,
+						mode: event.request.mode,
+						credentials: event.request.credentials,
+						referrer: event.request.referrer,
+						redirect: 'follow',
+						body: body
+					} );
 
-				// Add request to queue. @todo Replace when upgrading to Workbox v4!
-				queue.addRequest( queuedRequest );
+					// Add request to queue. @todo Replace when upgrading to Workbox v4!
+					queue.addRequest( queuedRequest );
 
-				const jsonBody = JSON.stringify( { 'error': errorMessages.comment } );
-				return new Response( jsonBody, {
-					headers: {
-						'Access-Control-Allow-Origin': SITE_URL,
-						'Access-Control-Allow-Credentials': 'true',
-						'Content-Type': 'application/json; charset=UTF-8',
-						'Access-Control-Expose-Headers': 'AMP-Access-Control-Allow-Source-Origin',
-						'AMP-Access-Control-Allow-Source-Origin': SITE_URL,
-						'Cache-Control': 'no-cache, must-revalidate, max-age=0'
-					}
+					const jsonBody = JSON.stringify( { 'error': errorMessages.comment } );
+					return new Response( jsonBody, {
+						status: 202,
+						statusText: 'Accepted',
+						headers: {
+							'Access-Control-Allow-Origin': SITE_URL,
+							'Access-Control-Allow-Credentials': 'true',
+							'Content-Type': 'application/json; charset=UTF-8',
+							'Access-Control-Expose-Headers': 'AMP-Access-Control-Allow-Source-Origin',
+							'AMP-Access-Control-Allow-Source-Origin': SITE_URL,
+							'Cache-Control': 'no-cache, must-revalidate, max-age=0'
+						}
+					} );
 				} );
 			} );
 	};
