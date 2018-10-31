544a747d41542e9aeecb2642a707d8199e77596d
Miina Sikk <miina.sikk@gmail.com>
2018-10-30 21:31:01 -0300

    Merge comments component back into navigation component.

--- wp-includes/components/class-wp-service-worker-navigation-routing-component.php
+++ wp-includes/components/class-wp-service-worker-navigation-routing-component.php
@@ -359,10 +359,18 @@ class WP_Service_Worker_Navigation_Routing_Component implements WP_Service_Worke
 		}
 
 		$scripts->register(
+			'wp-offline-commenting',
+			array(
+				'src'  => array( $this, 'get_offline_commenting_script' ),
+				'deps' => array( 'wp-base-config' ),
+			)
+		);
+
+		$scripts->register(
 			'wp-navigation-routing',
 			array(
 				'src'  => array( $this, 'get_script' ),
-				'deps' => array( 'wp-base-config' ),
+				'deps' => array( 'wp-base-config', 'wp-offline-commenting' ),
 			)
 		);
 
@@ -477,6 +485,22 @@ class WP_Service_Worker_Navigation_Routing_Component implements WP_Service_Worke
 	 */
 	public function get_script() {
 		$script = file_get_contents( PWA_PLUGIN_DIR . '/wp-includes/js/service-worker-navigation-routing.js' ); // phpcs:ignore WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents
+		$script = preg_replace( '#/\*\s*global.+?\*/#', '', $script );
+
+		return str_replace(
+			array_keys( $this->replacements ),
+			array_values( $this->replacements ),
+			$script
+		);
+	}
+
+	/**
+	 * Get script for offline commenting requests.
+	 *
+	 * @return string Script.
+	 */
+	public function get_offline_commenting_script() {
+		$script = file_get_contents( PWA_PLUGIN_DIR . '/wp-includes/js/service-worker-offline-commenting.js' ); // phpcs:ignore WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents
 		$script = preg_replace( '#/\*\s*global.+?\*/#', '', $script );
 
 		return str_replace(
