44647f29276102909f5adfa518ff91d73bb8f6fe
Ari <aristath@gmail.com>
2019-02-05 16:53:51 +0200

    Use array_shift. See #131

--- wp-includes/class-wp-web-app-manifest.php
+++ wp-includes/class-wp-web-app-manifest.php
@@ -93,10 +93,7 @@ class WP_Web_App_Manifest {
 		if ( current_theme_supports( 'theme-color' ) ) {
 			$theme_color = get_theme_support( 'theme-color' );
 			if ( $theme_color ) {
-				if ( is_array( $theme_color ) && ! empty( $theme_color[0] ) ) {
-					return $theme_color[0];
-				}
-				return $theme_color;
+				return ( is_array( $theme_color ) ) ? array_shift( $theme_color ) : $theme_color;
 			}
 		}
 
