8066a733a1201b0b28059b0c3855f4b79e0c292a
Weston Ruter <westonruter@google.com>
2019-03-27 17:39:24 -0700

    Prevent 404 handling for error template requests

--- wp-includes/class-wp.php
+++ wp-includes/class-wp.php
@@ -19,3 +19,22 @@ function pwa_add_error_template_query_var() {
 	$wp->add_query_var( WP_Service_Workers::QUERY_VAR );
 }
 add_action( 'init', 'pwa_add_error_template_query_var' );
+
+/**
+ * Prevent handling an offline template request as a 404 when there are no posts published.
+ *
+ * For a core merge, this logic should be incorporated into `WP::handle_404()`.
+ *
+ * @see \WP::handle_404()
+ *
+ * @param bool     $preempt  Whether to short-circuit default header status handling. Default false.
+ * @param WP_Query $wp_query WordPress Query object.
+ * @return bool
+ */
+function pwa_filter_pre_handle_404_for_error_template_requests( $preempt, WP_Query $wp_query ) {
+	if ( $wp_query->get( 'wp_error_template' ) ) {
+		$preempt = true;
+	}
+	return $preempt;
+}
+add_filter( 'pre_handle_404', 'pwa_filter_pre_handle_404_for_error_template_requests', 10, 2 );
