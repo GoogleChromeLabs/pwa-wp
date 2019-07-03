a5d131beb5d8a4e67aa879706c4afadde487cf46
Weston Ruter <westonruter@google.com>
2019-07-03 16:50:40 -0700

    Set the admin cookie path to be the scope

--- wp-includes/service-workers.php
+++ wp-includes/service-workers.php
@@ -163,7 +163,7 @@ function wp_print_service_workers() {
 							<?php echo wp_json_encode( compact( 'scope' ) ); ?>
 						).then( reg => {
 							<?php if ( WP_Service_Workers::SCOPE_ADMIN === $name ) : ?>
-								document.cookie = 'wordpress_sw_installed=1; path=/; expires=Fri, 31 Dec 9999 23:59:59 GMT; secure; samesite=strict';
+								document.cookie = <?php echo wp_json_encode( sprintf( 'wordpress_sw_installed=1; path=%s; expires=Fri, 31 Dec 9999 23:59:59 GMT; secure; samesite=strict', $scope ) ); ?>;
 							<?php endif; ?>
 							<?php if ( ! wp_service_worker_skip_waiting() ) : ?>
 								reg.addEventListener( 'updatefound', () => {
