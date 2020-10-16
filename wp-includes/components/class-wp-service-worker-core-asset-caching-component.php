2cea537d65ee16fc581d0ff9e8995433ca73bebe
Weston Ruter <westonruter@google.com>
2020-10-16 13:09:53 -0700

    Add caching of emoji from s.w.org

--- wp-includes/components/class-wp-service-worker-core-asset-caching-component.php
+++ wp-includes/components/class-wp-service-worker-core-asset-caching-component.php
@@ -31,8 +31,13 @@ class WP_Service_Worker_Core_Asset_Caching_Component implements WP_Service_Worke
 			return;
 		}
 
+		$cached_base_url_patterns = array(
+			preg_quote( trailingslashit( includes_url() ), '/' ) . '.*',
+			'https?:\/\/s\.w\.org\/images\/core\/emoji\/.*',
+		);
+
 		$scripts->caching_routes()->register(
-			'^' . preg_quote( trailingslashit( includes_url() ), '/' ) . '.*',
+			'^(' . implode( '|', $cached_base_url_patterns ) . ')',
 			array(
 				// Even though assets should have far-future expiration, network-first is still preferred for development purposes.
 				'strategy'  => WP_Service_Worker_Caching_Routes::STRATEGY_NETWORK_FIRST,
