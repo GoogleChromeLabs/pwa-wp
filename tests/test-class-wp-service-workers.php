2ba26617d71652348b1df282a16b14efc8f8d528
Weston Ruter <westonruter@google.com>
2021-08-22 21:08:09 -0700

    Replace deprecated assertNotContains() with `assertStringNotContainsString()`

--- tests/test-class-wp-service-workers.php
+++ tests/test-class-wp-service-workers.php
@@ -71,7 +71,7 @@ class Test_WP_Service_Workers extends WP_UnitTestCase {
 		$this->assertSame( 0, get_current_user_id() );
 		$this->assertStringContainsString( $this->return_foo_sw(), $output );
 		$this->assertStringContainsString( $this->return_bar_sw(), $output );
-		$this->assertNotContains( $this->return_baz_sw(), $output );
+		$this->assertStringNotContainsString( $this->return_baz_sw(), $output );
 		$this->assertTrue(
 			strpos( $output, $this->return_foo_sw() ) < strpos( $output, $this->return_bar_sw() )
 		);
@@ -97,7 +97,7 @@ class Test_WP_Service_Workers extends WP_UnitTestCase {
 
 		$this->assertSame( 0, get_current_user_id() );
 		$this->assertStringContainsString( $this->return_foo_sw(), $output );
-		$this->assertNotContains( $this->return_bar_sw(), $output );
+		$this->assertStringNotContainsString( $this->return_bar_sw(), $output );
 		$this->assertStringContainsString( $this->return_baz_sw(), $output );
 		$this->assertTrue(
 			strpos( $output, $this->return_foo_sw() ) < strpos( $output, $this->return_baz_sw() )
