c752b6fe7d7f9af016964e6fdc011a53970790d8
Ankit Gade <ankit.gade@rtcamp.com>
2022-02-11 22:35:55 +0530

    Fix: Icon toggling

---
+++ tests/wp-admin/test-admin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Tests for admin related functions and hooks.
+ *
+ * @package PWA
+ */
+
+use Yoast\WPTestUtils\WPIntegration\TestCase;
+
+/**
+ * Tests for class WP_Service_Worker_Scripts.
+ */
+class Test_WP_Service_Worker_Scripts extends TestCase {
+
+}
