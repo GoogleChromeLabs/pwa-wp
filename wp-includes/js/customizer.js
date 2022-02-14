6d18007c89ea4403b071d91831bbad552425e6e3
Ankit Gade <ankit.gade@rtcamp.com>
2022-02-14 12:36:03 +0530

    Fix: Linting

--- wp-includes/js/customizer.js
+++ wp-includes/js/customizer.js
@@ -1,11 +1,12 @@
-/* global PWA_Customizer_Data, console, document, wp */
+/* global PWA_Customizer_Data */
 
-(function() {
-
-	wp.customize.bind('ready', function() {
+(function () {
+	wp.customize.bind('ready', function () {
 		let siteIcon = PWA_Customizer_Data.siteIcon;
 		// Checkbox input.
-		const maskableInput = document.getElementById( '_customize-input-pwa_maskable_icon' );
+		const maskableInput = document.getElementById(
+			'_customize-input-pwa_maskable_icon'
+		);
 
 		/**
 		 * Listens to icon update. This includes following scenarios.
@@ -15,36 +16,38 @@
 		 * 3. Icon set as maskable.
 		 * 4. Icon set as un-maskable.
 		 */
-		const iconUpdateListener = function() {
-			const iconPreview = document.querySelector('img.app-icon-preview');
-
-			siteIcon = parseInt( this.iconId, 10 );
+		const iconUpdateListener = function () {
+			siteIcon = parseInt(this.iconId, 10);
 			maskableInput.value = this.checked;
 			maskableInput.checked = this.checked;
 
-			if( ! siteIcon ) {
+			if (!siteIcon) {
 				//alert( wp.customize.control( 'pwa_maskable_icon' ) );
-				wp.customize.control( 'pwa_maskable_icon' ).deactivate();
+				wp.customize.control('pwa_maskable_icon').deactivate();
 				return;
 			}
 
 			// At this point we are sure that icon is set, thus activate control.
-			wp.customize.control( 'pwa_maskable_icon' ).activate();
+			wp.customize.control('pwa_maskable_icon').activate();
 
-			if ( iconPreview ) {
-				document.querySelector('img.app-icon-preview').style.clipPath = this.checked && siteIcon ? 'inset(10% round 50%)' : '';
+			const iconPreview = document.querySelector('img.app-icon-preview');
+
+			if (iconPreview) {
+				document.querySelector('img.app-icon-preview').style.clipPath =
+					this.checked && siteIcon ? 'inset(10% round 50%)' : '';
 			}
 		};
 
 		/**
 		 * Bind to the events when icon is updated or removed.
 		 */
-		wp.customize( 'site_icon', function (value) {
-			value.bind(function ( id ) {
+		wp.customize('site_icon', function (value) {
+			value.bind(function (id) {
 				iconUpdateListener.call({
 					iconId: id,
 					// If image is removed or changed, uncheck maskable checkbox.
-					checked: ( id && id === siteIcon ) ? isMaskableIcon.checked : false,
+					checked:
+						id && id === siteIcon ? maskableInput.checked : false,
 				});
 			});
 		});
@@ -52,11 +55,11 @@
 		/**
 		 * Bind the checkbox change event.
 		 */
-		wp.customize( 'pwa_maskable_icon', function (value) {
-			value.bind(function ( checked ) {
+		wp.customize('pwa_maskable_icon', function (value) {
+			value.bind(function (checked) {
 				iconUpdateListener.call({
 					iconId: siteIcon,
-					checked: checked,
+					checked,
 				});
 			});
 		});
