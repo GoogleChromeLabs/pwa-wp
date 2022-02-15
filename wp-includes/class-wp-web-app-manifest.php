9c18edad00820cb0a2732fb1916d415c1fc264ce
Weston Ruter <westonruter@google.com>
2022-02-14 21:01:31 -0800

    Fix logic inversion for maskable icon

--- wp-includes/class-wp-web-app-manifest.php
+++ wp-includes/class-wp-web-app-manifest.php
@@ -236,23 +236,6 @@ final class WP_Web_App_Manifest {
 		}
 
 		/**
-		 * If user has not confirmed site icon as maskable, add icon purpose as `any maskable`.
-		 * By default, icons are maskable and lighthouse audits fails if icon is not really maskable.
-		 *
-		 * See <https://web.dev/maskable-icon-audit/>
-		 */
-		$manifest_icon_maskable = get_option( 'site_icon_maskable', false );
-		if ( ! $manifest_icon_maskable && isset( $manifest['icons'] ) ) {
-			$manifest['icons'] = array_map(
-				function ( $icon ) {
-					$icon['purpose'] = 'any maskable';
-					return $icon;
-				},
-				$manifest['icons']
-			);
-		}
-
-		/**
 		 * Enables overriding the manifest json.
 		 *
 		 * There are more possible values for this, including 'orientation' and 'scope.'
@@ -413,16 +396,27 @@ final class WP_Web_App_Manifest {
 			return array();
 		}
 
-		$icons     = array();
 		$mime_type = get_post_mime_type( $site_icon_id );
+		if ( ! $mime_type ) {
+			return array();
+		}
+
+		$icons    = array();
+		$maskable = get_option( 'site_icon_maskable', false );
 		foreach ( $this->default_manifest_icon_sizes as $size ) {
 			$src = get_site_icon_url( $size );
 			if ( $src ) {
-				$icons[] = array(
+				$icon = array(
 					'src'   => $src,
 					'sizes' => sprintf( '%1$dx%1$d', $size ),
 					'type'  => $mime_type,
 				);
+
+				if ( $maskable ) {
+					$icon['purpose'] = 'any maskable';
+				}
+
+				$icons[] = $icon;
 			}
 		}
 		return $icons;
