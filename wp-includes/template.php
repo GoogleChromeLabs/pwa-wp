c831d406722f27b34daab94cb8c263050a0b5421
Weston Ruter <westonruter@google.com>
2022-04-14 16:39:58 -0700

    Remove dead wp_service_worker_offline_page_reload() code that checks request method

--- wp-includes/template.php
+++ wp-includes/template.php
@@ -178,10 +178,6 @@ function wp_service_worker_error_message_placeholder() {
  * @since 0.7
  */
 function wp_service_worker_offline_page_reload() {
-	if ( isset( $_SERVER['REQUEST_METHOD'] ) && 'GET' !== $_SERVER['REQUEST_METHOD'] ) {
-		return;
-	}
-
 	if ( ! is_offline() && ! is_500() ) {
 		return;
 	}
