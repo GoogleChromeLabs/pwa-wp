54ed60b487644afc350417cf26507265e5050921
Weston Ruter <westonruter@google.com>
2022-04-14 20:42:36 -0700

    Update test_wp_service_worker_offline_page_reload

--- tests/test-template.php
+++ tests/test-template.php
@@ -11,6 +11,7 @@ use Yoast\WPTestUtils\WPIntegration\TestCase;
  * Tests for template.php
  */
 class Test_Template extends TestCase {
+
 	/**
 	 * Test if function adding script on GET method, is_offline() or is_500 is true
 	 *
@@ -21,8 +22,7 @@ class Test_Template extends TestCase {
 		$this->assertEquals( 10, has_action( 'error_footer', 'wp_service_worker_offline_page_reload' ) );
 
 		// Check when method is GET but not offline or 500.
-		$actual_script = wp_service_worker_offline_page_reload();
-		$this->assertEquals( $_SERVER['REQUEST_METHOD'], 'GET' );
+		$actual_script = get_echo( 'wp_service_worker_offline_page_reload' );
 		$this->assertFalse( is_offline() );
 		$this->assertFalse( is_500() );
 		$this->assertEmpty( $actual_script );
@@ -31,36 +31,20 @@ class Test_Template extends TestCase {
 		$error_template_url = add_query_arg( 'wp_error_template', 'offline', home_url( '/', 'relative' ) );
 		$this->go_to( $error_template_url );
 
-		ob_start();
-		wp_service_worker_offline_page_reload();
-		$actual_script = ob_get_clean();
-		$this->assertEquals( $_SERVER['REQUEST_METHOD'], 'GET' );
+		$actual_script = get_echo( 'wp_service_worker_offline_page_reload' );
 		$this->assertTrue( is_offline() );
 		$this->assertFalse( is_500() );
 		$this->assertStringContainsString( '<script type="module">', $actual_script );
-		$this->assertStringContainsString( 'await fetch(location.href, {method: \'HEAD\'})', $actual_script );
+		$this->assertStringContainsString( 'await fetch', $actual_script );
 
 		// Check if script is added when 500.
 		$error_template_url = add_query_arg( 'wp_error_template', '500', home_url( '/', 'relative' ) );
 		$this->go_to( $error_template_url );
 
-		ob_start();
-		wp_service_worker_offline_page_reload();
-		$actual_script = ob_get_clean();
-		$this->assertEquals( $_SERVER['REQUEST_METHOD'], 'GET' );
+		$actual_script = get_echo( 'wp_service_worker_offline_page_reload' );
 		$this->assertFalse( is_offline() );
 		$this->assertTrue( is_500() );
 		$this->assertStringContainsString( '<script type="module">', $actual_script );
-		$this->assertStringContainsString( 'await fetch(location.href, {method: \'HEAD\'})', $actual_script );
-
-		$this->go_to( home_url( '/', 'relative' ) );
-
-		// Check when method is not GET.
-		$_SERVER['REQUEST_METHOD'] = 'POST';
-		$actual_script             = wp_service_worker_offline_page_reload();
-		$this->assertEquals( $_SERVER['REQUEST_METHOD'], 'POST' );
-		$this->assertFalse( is_offline() );
-		$this->assertFalse( is_500() );
-		$this->assertEmpty( $actual_script );
+		$this->assertStringContainsString( 'await fetch', $actual_script );
 	}
 }
