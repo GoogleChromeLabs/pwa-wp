cee31952bfdd0354fa4df982baa67fd51779dd06
Weston Ruter <westonruter@google.com>
2022-04-22 17:41:19 -0700

    Revert "Opt to fully control siteIconMaskable.active value client-side"
    This reverts commit 4136f52d26f20cff7822e44a7127052758f9e30b.

--- wp-includes/class-wp-customize-manager.php
+++ wp-includes/class-wp-customize-manager.php
@@ -35,10 +35,13 @@ function pwa_customize_register_site_icon_maskable( WP_Customize_Manager $wp_cus
 		$wp_customize->add_control(
 			'site_icon_maskable',
 			array(
-				'type'     => 'checkbox',
-				'section'  => 'title_tagline',
-				'label'    => __( 'Maskable icon', 'pwa' ),
-				'priority' => $site_icon_control->priority + 1,
+				'type'            => 'checkbox',
+				'section'         => 'title_tagline',
+				'label'           => __( 'Maskable icon', 'pwa' ),
+				'priority'        => $site_icon_control->priority + 1,
+				'active_callback' => function() use ( $wp_customize ) {
+					return (bool) $wp_customize->get_setting( 'site_icon' )->value();
+				},
 			)
 		);
 	}
